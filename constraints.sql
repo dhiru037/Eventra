ea2ae4770c7fc7f9aa9cb3dfa3594fe3048d2206
Chandana Mulagund <99470852+chandanamulagund@users.noreply.github.com>
2023-11-23 15:17:32 +0530

    SQL table constraints

---
+++ constraints.sql
@@ -0,0 +1,8 @@
+use project1;
+alter table participant add constraint fk_part2event foreign key(event_id) references event(event_id);
+alter table event add constraint fk_event2club foreign key(club_id) references club(club_id);
+alter table faculty add constraint fk_faculty2club foreign key(club_id) references club(club_id);
+alter table club_head add constraint fk_clubhead2club foreign
+key(club_id) references club(club_id);
+alter table club add constraint fk_club2fac foreign key(fac_id) references faculty(faculty_id);
+alter table club add constraint fk_club2clubhead foreign key(headed_by) references club_head(head_id);
