1ec1cd806ec7c5d78312b813fea8e0503fc013f9
Chandana Mulagund <99470852+chandanamulagund@users.noreply.github.com>
2023-11-24 01:07:13 +0530

    dropped price from event table

--- constraints.sql
+++ constraints.sql
@@ -6,3 +6,9 @@ alter table club_head add constraint fk_clubhead2club foreign
 key(club_id) references club(club_id);
 alter table club add constraint fk_club2fac foreign key(fac_id) references faculty(faculty_id);
 alter table club add constraint fk_club2clubhead foreign key(headed_by) references club_head(head_id);
+
+ALTER TABLE club ADD COLUMN image_url VARCHAR(255);
+ALTER TABLE faculty ADD COLUMN image_url VARCHAR(255);
+ALTER TABLE club_head ADD COLUMN image_url VARCHAR(255);
+
+ALTER TABLE event drop column price;
